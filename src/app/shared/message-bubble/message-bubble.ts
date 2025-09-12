470201beb1b45289aea82537796744552c39af4a
Abhishek <abhishekkumargupta20112000@gmail.com>
2025-09-12 17:45:43 +0530

    Initial setup

---
+++ src/app/shared/message-bubble/message-bubble.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-message-bubble',
+  imports: [],
+  templateUrl: './message-bubble.html',
+  styleUrl: './message-bubble.scss'
+})
+export class MessageBubble {
+
+}
