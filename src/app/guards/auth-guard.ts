b184edab3e3906e99f9ab77c19c0f008e8e67fe0
Abhishek <abhishekkumargupta20112000@gmail.com>
2025-09-19 19:12:21 +0530

    disable authguard temporarely

--- src/app/guards/auth-guard.ts
+++ src/app/guards/auth-guard.ts
@@ -2,17 +2,17 @@ import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  // const router = inject(Router);
+  const router = inject(Router);
 
-  // const isLoggedIn = !!localStorage.getItem('token'); // Example check
-  // // ******* change it with user object stored in localStorage ******//
-  // if (isLoggedIn) {
-  //   console.log("Token Found");
-  //   return true;
-  // } else {
-  //   console.log("Token Not Found");
-  //   router.navigate(['/auth/login']);
-  //   return false;
-  // }
+  const isLoggedIn = !!localStorage.getItem('token'); // Example check
+  // ******* change it with user object stored in localStorage ******//
+  if (isLoggedIn) {
+    console.log("Token Found");
     return true;
+  } else {
+    console.log("Token Not Found");
+    router.navigate(['/auth/login']);
+    // return false;
+    return true;
+  }
 };
