3a1b854c00c759532b0006fb59c0212fd1b70366
Abhishek <abhishekkumargupta20112000@gmail.com>
2025-11-06 20:28:20 +0530

    logout setup

--- src/app/shared/navbar/navbar.ts
+++ src/app/shared/navbar/navbar.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Router } from '@angular/router';
+import { AuthService } from "../../services/auth.service";
 
 @Component({
   selector: 'app-navbar',
@@ -11,7 +12,14 @@ import { RouterModule } from '@angular/router';
 export class Navbar {
   mobileMenuOpen = false;
 
+  constructor(private authService: AuthService, private router: Router){}
+
   toggleMenu() {
     this.mobileMenuOpen = !this.mobileMenuOpen;
+  }
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/welcome']);
   }
 }
